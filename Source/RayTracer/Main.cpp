22f332267f494bb97845d1934dab5df9da69ff04
Hergusbergus <zane.ammerman@gmail.com>
2023-11-28 14:41:18 -0700

    Added all the code.

--- Source/RayTracer/Main.cpp
+++ Source/RayTracer/Main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <SDL.h>
+#include "Renderer.h"
 
 int main() {
     std::cout << "Hello, world!" << std::endl;
+
+    Renderer renderer;
+    renderer.Initialize();
+    renderer.CreateWindow("Ray Tracing Program", 400, 300);
+
+    bool quit = false;
+    while (!quit)
+    {
+        SDL_Event event;
+        SDL_PollEvent(&event);
+        switch (event.type)
+        {
+        case SDL_QUIT:
+            quit = true;
+            break;
+        }
+    }
+
+    renderer.Shutdown();
+
     return 0;
 }
--- Source/RayTracer/Renderer.h
+++ Source/RayTracer/Renderer.h
@@ -1,5 +1,18 @@
 #pragma once
+#include <iostream>
+#include <SDL.h>
+
 class Renderer
 {
+public:
+	Renderer() = default;
+
+	bool Initialize();
+	void Shutdown();
+	bool CreateWindow(const std::string& title, int width, int height);
+
+private:
+	SDL_Window* window = nullptr;
+	SDL_Renderer* renderer = nullptr;
 };
 
